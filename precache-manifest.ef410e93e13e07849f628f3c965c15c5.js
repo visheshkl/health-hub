acda17278af222a60ec3c3982ee03b669c86e776
Vishesh Khandelwal <visheshkkl@gmail.com>
2020-03-16 16:35:05 +0530

    Updates

---
+++ precache-manifest.ef410e93e13e07849f628f3c965c15c5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0be1695c9a45ed6fd050a1fc42a6d180",
+    "url": "/health-hub/index.html"
+  },
+  {
+    "revision": "65f8e41a1459b65fe13b",
+    "url": "/health-hub/static/css/2.fa748e1e.chunk.css"
+  },
+  {
+    "revision": "e2a796fcd2525b8f56de",
+    "url": "/health-hub/static/css/main.a9f7377a.chunk.css"
+  },
+  {
+    "revision": "65f8e41a1459b65fe13b",
+    "url": "/health-hub/static/js/2.0bb1cb44.chunk.js"
+  },
+  {
+    "revision": "87be874c5a5fb2b9f6cce09cd6792043",
+    "url": "/health-hub/static/js/2.0bb1cb44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2a796fcd2525b8f56de",
+    "url": "/health-hub/static/js/main.674d7ef7.chunk.js"
+  },
+  {
+    "revision": "52d97a24c04a6e228086",
+    "url": "/health-hub/static/js/runtime-main.a30db993.js"
+  },
+  {
+    "revision": "4bced7c4c0d61d4f988629bb8ae80b8b",
+    "url": "/health-hub/static/media/fa-solid-900.4bced7c4.woff"
+  },
+  {
+    "revision": "acf50f59802f20d8b45220eaae532a1c",
+    "url": "/health-hub/static/media/fa-solid-900.acf50f59.ttf"
+  },
+  {
+    "revision": "b557f56e367e59344ca95f9d1fb44352",
+    "url": "/health-hub/static/media/fa-solid-900.b557f56e.svg"
+  },
+  {
+    "revision": "b5cf8ae26748570d8fb95a47f46b69e1",
+    "url": "/health-hub/static/media/fa-solid-900.b5cf8ae2.woff2"
+  },
+  {
+    "revision": "ef3df98419d143d9617fe163bf4edc0b",
+    "url": "/health-hub/static/media/fa-solid-900.ef3df984.eot"
+  },
+  {
+    "revision": "f59f35a62777a177160ed387c9cd3db5",
+    "url": "/health-hub/static/media/logo.f59f35a6.png"
+  },
+  {
+    "revision": "eb39e7c700f11d11668b1cedb56b0eea",
+    "url": "/health-hub/static/media/mail-new.eb39e7c7.svg"
+  }
+]);
